36644cf74b3f3f3f885754cd5dd5896b22d73911
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2018-11-16 10:53:06 -0300

    Branch master

--- Git-Aula-20182/src/HelloWorld.java
+++ Git-Aula-20182/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-		System.out.println("Mudança Bash");
+		System.out.println("Mudança Bash [branch master]");
 	
 		System.out.println("Mudança Eclipse!");
 		
